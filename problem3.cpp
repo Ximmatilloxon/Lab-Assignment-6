0041e5de5099fa43d0667398d5db2867da62d3e7
Ximmatilloxon Musayev <x.musayev@newuzbekistanuniversity.uz>
2024-04-08 09:59:36 +0500

    p3

--- problem3.cpp
+++ problem3.cpp
@@ -1,1 +1,22 @@
+#include "iostream"
+#include "map"
+using namespace std;
 
+int main(){
+    int n;
+    map<int, int> m;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int t;
+        cin >> t;
+        m[t]++;
+    }
+    int answer, Max = 0;
+    for (auto i:m) {
+        if (i.second > Max){
+            answer = i.first;
+            Max = i.second;
+        }
+    }
+    cout << answer;
+}
