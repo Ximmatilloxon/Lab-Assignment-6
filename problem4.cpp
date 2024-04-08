f6801cb3ca2f425bbe42c6425f2adfee0cd179c5
Ximmatilloxon Musayev <x.musayev@newuzbekistanuniversity.uz>
2024-04-08 11:13:06 +0500

    Done!

--- problem4.cpp
+++ problem4.cpp
@@ -1,10 +1,9 @@
 #include "iostream"
 #include "vector"
-#include "cstdint"
 using namespace std;
 
-void heapify(vector<int64_t> &arr, int64_t n, int64_t i){
-    int64_t largest = i, l = 2 * i + 1, r = 2 * i + 2;
+void heapify(vector<int> &arr, int n, int i){
+    int largest = i, l = 2 * i + 1, r = 2 * i + 2;
     if(l < n && arr[l] > arr[largest]) largest = l;
     if(r < n && arr[r] > arr[largest]) largest = r;
     if(largest != i){
@@ -13,34 +12,38 @@ void heapify(vector<int64_t> &arr, int64_t n, int64_t i){
     }
 }
 
-void heapSort(vector<int64_t > &arr, int64_t n){
-    for (int64_t i = n/2 - 1; i >=0; --i) {
+void heapSort(vector<int> &arr, int n){
+    for (int i = n/2 - 1; i >=0; --i) {
         heapify(arr, n, i);
     }
-    for (int64_t i = n - 1; i > 0; --i) {
+    for (int i = n - 1; i > 0; --i) {
         swap(arr[0], arr[i]);
         heapify(arr, i, 0);
     }
 }
 
 int main(){
-    int64_t n;
+    int n;
     cin >> n;
-    vector<int64_t> arr;
-    for (int64_t i = 0; i < n; ++i) {
+    vector<int> arr;
+    for (int i = 0; i < n; ++i) {
         int t;
         cin >> t;
         arr.push_back(t);
     }
     heapSort(arr, arr.size());
-    int64_t dif = INT_MAX;
-    for (int64_t i = 0; i < arr.size() - 1; ++i) dif = min(dif, arr[i + 1] - arr[i]);
-    vector<vector<int64_t >> vec;
-    for (int64_t i = 0; i < arr.size() - 1; ++i) {
+    int dif = INT_MAX;
+    for (int i = 0; i < arr.size(); ++i){
+        for (int j = 0; j < arr.size(); ++j) {
+            if(i != j) dif = min(dif, abs(arr[j] - arr[i]));
+        }
+    }
+    vector<vector<int>> vec;
+    for (int i = 0; i < arr.size() - 1; ++i) {
         if(arr[i + 1] - arr[i] == dif) vec.push_back({arr[i], arr[i+1]});
     }
     cout << vec[0][0] << " " << vec[0][1];
-    for (int64_t i = 1; i < vec.size(); ++i) {
+    for (int i = 1; i < vec.size(); ++i) {
         cout << "\n" << vec[i][0] << " " << vec[i][1];
     }
 }
